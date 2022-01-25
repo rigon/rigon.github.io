5f4043bb6f458b7d33e6f61bfb589f7144ca74ec
rigon <ricar.goncalves@gmail.com>
2022-01-25 01:29:43 +0000

    Base bootstrap v5.0 working template

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+// Load Bootstrap JS
+import 'bootstrap';
+import 'boxicons';
+import Aos from 'aos';
+import Typed from 'typed.js';
+
+// Load Styles
+import '../scss/main.scss';
+
+/**
+ * Easy selector helper function
+ */
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+
+Aos.init();
+
+/**
+ * Hero type effect
+ */
+const typed = select('.typed')
+if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+}
