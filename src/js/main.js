523486132633267e297ad01a7aa33d71657b3734
rigon <ricar.goncalves@gmail.com>
2022-01-25 15:13:00 +0000

    Improve overall styles and functionallity

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // Load Bootstrap JS
 import 'bootstrap';
 import 'boxicons';
-import Aos from 'aos';
+import AOS from 'aos';
 import Typed from 'typed.js';
 
 // Load Styles
@@ -18,8 +18,111 @@ const select = (el, all = false) => {
         return document.querySelector(el)
     }
 }
+/**
+ * Easy event listener function
+ */
+const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+        if (all) {
+            selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+            selectEl.addEventListener(type, listener)
+        }
+    }
+}
+/**
+ * Easy on scroll event listener 
+ */
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
 
-Aos.init();
+
+/**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = select('#navbar .scrollto', true)
+const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+            navbarlink.classList.add('active')
+        } else {
+            navbarlink.classList.remove('active')
+        }
+    })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+        top: elementPos,
+        behavior: 'smooth'
+    })
+}
+
+/**
+ * Back to top button
+ */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+            backtotop.classList.add('active')
+        } else {
+            backtotop.classList.remove('active')
+        }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+}
+
+/**
+ * Mobile nav toggle
+ */
+on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bx-menu')
+    this.classList.toggle('bx-x')
+})
+
+/**
+ * Scrool with ofset on links with a class name .scrollto
+ */
+on('click', '.scrollto', function (e) {
+    if (select(this.hash)) {
+        e.preventDefault()
+
+        let body = select('body')
+        if (body.classList.contains('mobile-nav-active')) {
+            body.classList.remove('mobile-nav-active')
+            let navbarToggle = select('.mobile-nav-toggle')
+            navbarToggle.classList.toggle('bx-menu')
+            navbarToggle.classList.toggle('bx-x')
+        }
+        scrollto(this.hash)
+    }
+}, true)
+
+/**
+ * Scroll with ofset on page load with hash links in the url
+ */
+window.addEventListener('load', () => {
+    if (window.location.hash) {
+        if (select(window.location.hash)) {
+            scrollto(window.location.hash)
+        }
+    }
+});
 
 /**
  * Hero type effect
@@ -36,3 +139,15 @@ if (typed) {
         backDelay: 2000
     });
 }
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener('load', () => {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    })
+});
